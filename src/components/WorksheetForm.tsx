2f8472db888f5e11937e1b69aecc6d2c543e2801
Jared Loosli <jloosli@gmail.com>
2025-09-13 04:29:34 -0600

    Make more mobile friendly

--- src/components/WorksheetForm.tsx
+++ src/components/WorksheetForm.tsx
@@ -126,7 +126,7 @@ export default function WorksheetForm() {
   };
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-2 md:p-8 pb-20 gap-16">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="flex flex-col gap-4 items-center sm:items-start">
           <h1 className="text-4xl font-bold">Mountain Flying Worksheet</h1>
